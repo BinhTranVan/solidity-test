1db8425797d65a13584c51e3ffec393265f9a6cf
BinhTranVan <binhtranvan31@gmail.com>
2025-07-04 14:56:46 +0700

    events-custom-logging

--- contract.sol
+++ contract.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
+    // Events for custom logging
+    event TokensBurned(address indexed burner, uint256 value);
+    event AllowanceSet(address indexed owner, address indexed spender, uint256 value);
+
     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
         _mint(msg.sender, initialSupply);
     }
